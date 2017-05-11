b939ea6e49298b5632c0754aa115bb88f28296c3
Tom Witkowski <dev.gummibeer@gmail.com>
2017-05-11 13:44:16 +0200

    styleci

--- src/MonologConfig/MonologConfigServiceProvider.php
+++ src/MonologConfig/MonologConfigServiceProvider.php
@@ -2,9 +2,9 @@
 
 namespace Astrotomic\MonologConfig;
 
-use Illuminate\Foundation\Application as LaravelApplication;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Lumen\Application as LumenApplication;
+use Illuminate\Foundation\Application as LaravelApplication;
 
 class MonologConfigServiceProvider extends ServiceProvider
 {
